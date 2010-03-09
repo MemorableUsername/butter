08c20f0b36e0c0ec5981dafd97b91a04292bab1c
dentist <avengingdentist@gmail.com>
2010-03-08 18:49:13 -0600

    * Add yaml configuration

--- buttbot.py
+++ buttbot.py
@@ -4,31 +4,25 @@ import random
 import time
 import re
 import fnmatch
-
-server = "irc.synirc.org"
-port = 6667
-nick = "buttebot"
+import yaml
 
 class ignore_list(list):
     _regex_mode = re.compile(r"^/(.*)/(i?)$")
     _glob_mode = re.compile(r"[\*\?\[\]]")
 
-    def __init__(self, filename):
-        f = open(filename)
-        for line in f:
-            line = line.strip()
-            m = self._regex_mode.search(line)
+    def __init__(self, enemies):
+        for i in enemies:
+            m = self._regex_mode.search(i)
             if m:
                 if len(m.group(2)) == 0:
                     self.append(re.compile(m.group(1)))
                 else:
                     self.append(re.compile(m.group(1), re.I))
                 continue
-            elif self._glob_mode.search(line):
-                self.append(re.compile( fnmatch.translate(line) ))
+            elif self._glob_mode.search(i):
+                self.append(re.compile( fnmatch.translate(i) ))
             else:
-                self.append(line)
-        f.close()
+                self.append(i)
 
     def __contains__(self, name):
         for i in self:
@@ -41,23 +35,37 @@ class ignore_list(list):
 
 
 class buttbot(irclib.SimpleIRCClient):
-    def __init__(self, server, port, nick, max_channels=5):
+    def __init__(self, config_file="config"):
         irclib.SimpleIRCClient.__init__(self)
-        self.connect(server, port, nick)
-        self.channels_left = max_channels
+        f = open(config_file)
+        config = yaml.load(f)
+        f.close()
+
+        self.connect(config['server'], config['port'], config['nick'])
+        self.default_channels = config['channels']
+        self.command = config['command']
+        try:
+            self.channels_left = config['max_channels']
+        except:
+            self.channels_left = 5
+
+        self.enemies = ignore_list(config['enemies'])
+
         self.last_butt = 0.0 # the epoch
 
-        self.ignore = ignore_list("ignore")
+    def on_welcome(self, connection, event):
+        for channel in self.default_channels:
+            connection.join(channel)
 
     def on_pubmsg(self, connection, event):
         msg = event.arguments()[0]
         user = event.source().split('!')[0]
         channel = event.target()
 
-        if user in self.ignore: return
+        if user in self.enemies: return
 
         bits = msg.split(' ', 1)
-        if bits[0] == "!butte":
+        if bits[0] == self.command:
             if len(msg) > 1:
                 try:
                     connection.privmsg(channel, user+": "+
@@ -80,5 +88,4 @@ class buttbot(irclib.SimpleIRCClient):
             self.channels_left -= 1
 
 if __name__ == "__main__":
-    irc = buttbot(server, port, nick)
-    irc.start()
+    buttbot().start()
