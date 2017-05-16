44fd81e8026102130579c27df43f145d6a554cf2
MemorableUsername <wrathofthenoob@gmail.com>
2017-05-15 21:44:41 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='butter3',
+      version='0.1',
+      description='The world-class buttifying plugin for python3, at least until dentist merges ' +
+                  'nikolas\'s pull request',
+      url='https://github.com/MemorableUsername/butter',
+      author='',
+      author_email='',
+      license='MIT',
+      zip_safe=False)
